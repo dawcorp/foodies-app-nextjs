827fc9b5dd0815cb7a76ec101f2df9038be1b517
Davide Voza <davide.voza@mozart.it>
2025-02-07 16:47:02 +0100

    feat: implement meals loading page and enhance meals layout with dynamic data fetching

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,17 +1,27 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals page</h1>
-      <ul>
-        <li>
-          <Link href="/meals/first-meal">First meal</Link>
-        </li>
-        <li>
-          <Link href="/meals/second-meal">Second meal</Link>
-        </li>
-      </ul>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
