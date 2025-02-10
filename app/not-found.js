6fd3763eea28fa8e58ed86e8b5c1d23ed6a2acde
Davide Voza <davide.voza@mozart.it>
2025-02-10 18:12:20 +0100

    feat: enhance meal details page with dynamic data fetching and improved layout; remove loading page and styles

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Ops.. Page not found!</h1>
+      <p>Unfortunately, we could not finde the requested page.</p>
+    </main>
+  );
+}
